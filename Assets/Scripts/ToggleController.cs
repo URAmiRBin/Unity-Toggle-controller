63d96239ac970e3b018826bf6bf06e0ea44ace34
Amir Bin <ur.amirbin@gmail.com>
2021-04-08 10:56:15 +0430

    Remove on and off icons

--- Assets/Scripts/ToggleController.cs
+++ Assets/Scripts/ToggleController.cs
@@ -22,10 +22,6 @@ public class ToggleController : MonoBehaviour
 
 	public float handleOffset;
 
-	public GameObject onIcon;
-	public GameObject offIcon;
-
-
 	public float speed;
 	static float t = 0.0f;
 
@@ -50,15 +46,11 @@ public class ToggleController : MonoBehaviour
 		{
 			toggleBgImage.color = onColorBg;
 			handleTransform.localPosition = new Vector3(onPosX, 0f, 0f);
-			onIcon.gameObject.SetActive(true);
-			offIcon.gameObject.SetActive(false);
 		}
 		else
 		{
 			toggleBgImage.color = offColorBg;
 			handleTransform.localPosition = new Vector3(offPosX, 0f, 0f);
-			onIcon.gameObject.SetActive(false);
-			offIcon.gameObject.SetActive(true);
 		}
 	}
 		
@@ -84,25 +76,15 @@ public class ToggleController : MonoBehaviour
 
 
 	public void Toggle(bool toggleStatus)
-	{
-		if(!onIcon.active || !offIcon.active)
-		{
-			onIcon.SetActive(true);
-			offIcon.SetActive(true);
-		}
-		
+	{	
 		if(toggleStatus)
 		{
 			toggleBgImage.color = SmoothColor(onColorBg, offColorBg);
-			Transparency (onIcon, 1f, 0f);
-			Transparency (offIcon, 0f, 1f);
 			handleTransform.localPosition = SmoothMove(handle, onPosX, offPosX);
 		}
 		else 
 		{
 			toggleBgImage.color = SmoothColor(offColorBg, onColorBg);
-			Transparency (onIcon, 0f, 1f);
-			Transparency (offIcon, 1f, 0f);
 			handleTransform.localPosition = SmoothMove(handle, offPosX, onPosX);
 		}
 			
